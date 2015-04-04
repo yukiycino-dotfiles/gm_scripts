74a60b06859f259a75c91ee37c52668fbf4b27ae
yuki_yano <yuki.ycino@gmail.com>
2015-04-04 20:38:29 +0900

    update Google_Search_Tools_Back

--- Google_Search_Tools_Back/152796.user.js
+++ Google_Search_Tools_Back/152796.user.js
@@ -1,9 +1,9 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name        Google Search Tools Back
 // @namespace   http://akr.tw/
-// @version     1.6.5
+// @version     1.7.3
 //
-// @description Brings back Google Search tools.
+// @description Brings back Google search tools to left side and displays drop-down menu items.
 // @author      Ming-Hsien Lin (akiratw)
 // @license     MIT License
 //
@@ -30,219 +30,291 @@
 // @grant       GM_addStyle
 // ==/UserScript==
 
-var GM_addStyle = GM_addStyle || function (css) {
-  var style = document.createElement('style');
-  style.setAttribute('type', 'text/css');
-  style.textContent = css;
-  document.head.appendChild(style);
-};
+(function () {
+  var css =
+    '/**' +
+    ' * Horizontal tabs - Display all tabs.' +
+    ' */' +
 
-GM_addStyle(
-  '#hdtb_more,' +
-  '#hdtb_tls {' +
-  '	display: none !important;' +
-  '}' +
+    '#hdtb_more {' +
+    '    display: none !important;' +
+    '}' +
 
-  '#hdtb_more_mn {' +
-  '	display: inline-block !important;' +
-  '	top: 1px !important;' +
-  '	border: none !important;' +
-  '	box-shadow: none !important;' +
-  '	background: transparent !important;' +
-  '}' +
+    '#hdtb_more_mn {' +
+    '    display: inline-block !important;' +
+    '    position: static !important;' +
+    '    border: 0 !important;' +
+    '    box-shadow: none !important;' +
+    '    background: transparent !important;' +
+    '}' +
 
-  '#cnt.mdm #hdtb_more_mn {' +
-  '	top: 10px !important;' +
-  '}' +
+    '#hdtb_more_mn .hdtb_mitem {' +
+    '    display: inline-block !important;' +
+    '}' +
 
-  '#hdtb_msb div.hdtb_mitem,' +
-  '#hdtb_msb div.hdtb_msb_hmi {' +
-  '	display: inline-block !important;' +
-  '}' +
+    '#hdtb_more_mn .hdtb_mitem a:hover {' +
+    '    background: transparent !important;' +
+    '}' +
 
-  '#hdtb_msb div.hdtb_mitem a:hover,' +
-  '#hdtb_msb div.hdtb_msb_hmi a:hover {' +
-  '	background: transparent !important;' +
-  '	color: #222 !important;' +
-  '}' +
+    '/**' +
+    ' * Search tools - Move to left side.' +
+    ' */' +
 
-  '#resultStats {' +
-  '	top: 0 !important;' +
-  '	opacity: 1 !important;' +
-  '}' +
+    '#hdtb_tls {' +
+    '    display: none !important;' +
+    '}' +
 
-  '#hdtbMenus {' +
-  '	display: block !important;' +
-  '	position: absolute !important;' +
-  '	top: 0 !important;' +
-  '	height: 0 !important;' +
-  '	overflow: visible !important;' +
-  '	background: transparent !important;' +
-  '}' +
+    '#hdtbMenus {' +
+    '    display: inline-block !important;' +
+    '    position: static !important;' +
+    '    float: left !important;' +
+    '    height: 0 !important;' +
+    '    overflow: visible !important;' +
+    '    background: transparent !important;' +
+    '}' +
 
-  '#hdtbMenus ul.hdtbU {' +
-  '	display: block !important;' +
-  '	position: relative !important;' +
-  '	top: 50px !important;' +
-  '	float: left !important;' +
-  '	clear: both !important;' +
-  '	width: 200px !important;' +
-  '	min-width: 200px !important;' +
-  '	max-width: 200px !important;' +
-  '	margin-top: 20px !important;' +
-  '	border: none !important;' +
-  '	box-shadow: none !important;' +
-  '}' +
+    '#hdtbMenus .hdtb-mn-cont {' +
+    '    height: 0 !important;' +
+    '}' +
 
-  '#hdtbMenus ul.hdtbU > li {' +
-  '	overflow: hidden !important;' +
-  '	white-space: normal !important;' +
-  '}' +
+    '#hdtbMenus .hdtb-mn-hd,' +
+    '#hdtbMenus .hdtbU {' +
+    '    display: block !important;' +
+    '    position: relative !important;' +
+    '    top: 0 !important;' +
+    '    width: 200px !important;' +
+    '    min-width: 200px !important;' +
+    '    max-width: 200px !important;' +
+    '    white-space: normal !important;' +
+    '}' +
 
-  '#hdtbMenus ul.hdtbU > li.hdtbSel,' +
-  '#hdtbMenus ul.hdtbU > li.hdtbSel > span {' +
-  '	background: none !important;' +
-  '	color: #DD4B39 !important;' +
-  '	font-weight: 700 !important;' +
-  '}' +
+    '#hdtbMenus .hdtbU {' +
+    '    margin-bottom: 10px !important;' +
+    '    border: 0 !important;' +
+    '    box-shadow: none !important;' +
+    '    background: transparent !important;' +
+    '}' +
 
-  '#hdtbMenus ul.hdtbU > li > span.hdtb-mn-c {' +
-  '	background: #FFF !important;' +
-  '	font-weight: 400 !important;' +
-  '	white-space: nowrap !important;' +
-  '}' +
+    '#hdtbMenus .hdtbU .hdtbItm.hdtbSel {' +
+    '    background: transparent !important;' +
+    '    color: #DD4B39 !important;' +
+    '    font-weight: bold !important;' +
+    '}' +
 
-  '#hdtbMenus span.tnv-lt-sm {' +
-  '	height: auto !important;' +
-  '	overflow: visible !important;' +
-  '}' +
+    '/* Sub menu. */' +
+    '#hdtbMenus span.tnv-lt-sm {' +
+    '    height: auto !important;' +
+    '    overflow: visible !important;' +
+    '    font-weight: normal !important;' +
+    '    white-space: nowrap !important;' +
+    '}' +
 
-  '#hdtbMenus > div > div {' +
-  '	display: none !important;' +
-  '}' +
+    '#hdtbMenus .hdtb-mn-hd,' +
+    '#hdtbMenus .hdtb-mn-hd .mn-dwn-arw {' +
+    '    display: none !important;' +
+    '}' +
 
-  '#lc-input {' +
-  '	display: block !important;' +
-  '	margin: 5px !important;' +
-  '}' +
+    '#hdtbMenus .hdtb-mn-hd .mn-hd-txt {' +
+    '    color: #DD4B39 !important;' +
+    '    white-space: normal !important;' +
+    '}' +
 
-  '#hdtb_rst.hdtb-mn-hd {' +
-  '	display: inline-block !important;' +
-  '	position: relative !important;' +
-  '	float: left !important;' +
-  '	clear: both !important;' +
-  '	margin: 60px 0 20px 15px !important;' +
-  '}' +
+    '#hdtbMenus .hdtb-mn-hd.hdtb-msel[aria-label*=" – "],' +
+    '#hdtbMenus .hdtb-mn-hd.hdtb-msel[aria-label*="×"] {' +
+    '    display: block !important;' +
+    '    padding-left: 30px !important;' +
+    '    pointer-events: none !important;' +
+    '}' +
 
-  'div.action-menu > a {' +
-  '	display: none !important;' +
-  '}' +
+    '#cdrlnk,' +
+    '.exylnk {' +
+    '    background: transparent !important;' +
+    '    color: inherit !important;' +
+    '}' +
 
-  'div.action-menu-panel {' +
-  '	display: inline-block !important;' +
-  '	position: static !important;' +
-  '	visibility: inherit !important;' +
-  '	border: none !important;' +
-  '	box-shadow: none !important;' +
-  '	background: transparent !important;' +
-  '}' +
+    '/* Location form. */' +
+    '#lc-input {' +
+    '    width: 100% !important;' +
+    '    max-width: 150px !important;' +
+    '    margin-right: 2px !important;' +
+    '    margin-bottom: 2px !important;' +
+    '}' +
 
-  'div.action-menu-panel ul li.action-menu-item {' +
-  '	display: inline-block !important;' +
-  '	margin: 2px !important;' +
-  '	background: #F1F1F1 !important;' +
-  '	color: #777 !important;' +
-  '}' +
+    '/* Reset button. */' +
+    '#hdtb_rst.hdtb-mn-hd {' +
+    '    display: block !important;' +
+    '    padding: 0 30px !important;' +
+    '}' +
 
-  'div.action-menu-panel ul li.action-menu-item:hover {' +
-  '	background: #F1F1F1 !important;' +
-  '}' +
+    '#resultStats {' +
+    '    top: 0 !important;' +
+    '    opacity: 1 !important;' +
+    '}' +
 
-  'div.action-menu-panel a.fl,' +
-  'div.action-menu-panel div.action-menu-button {' +
-  '	display: inline !important;' +
-  '	padding: 5px !important;' +
-  '	color: #777 !important;' +
-  '	font-size: 11px !important;' +
-  '}' +
+    '/* Align horizontal tabs and content. */' +
 
-  'div.action-menu-panel div.action-menu-button {' +
-  '	display: inline !important;' +
-  '	padding: 5px !important;' +
-  '	color: #777 !important;' +
-  '}' +
+    '#hdtb_s {' +
+    '    margin-left: 15px !important;' +
+    '}' +
 
-  '#center_col,' +
-  '#botabar,' +
-  '.ab_tnav_wrp {' +
-  '	margin-left: 200px !important;' +
-  '}' +
+    '#hdtb_msb > .hdtb_mitem:first-child,' +
+    '#center_col,' +
+    '#footcnt,' +
+    '.ab_tnav_wrp {' +
+    '    margin-left: 200px !important;' +
+    '}' +
 
-  '#hdtb_msb > .hdtb_mitem:first-child,' +
-  '#hdtb_msb > .hdtb_mitem.hdtb_msel:first-child {' +
-  '	margin-left: 215px !important;' +
-  '}' +
+    '#cnt,' +
+    '#footcnt,' +
+    '#footcnt .fbar {' +
+    '    background: transparent !important;' +
+    '}' +
 
-  '@media (max-width: 1200px) {' +
-  '	#hdtb_msb > .hdtb_mitem:first-child,' +
-  '	#hdtb_msb > .hdtb_mitem.hdtb_msel:first-child {' +
-  '		margin-left: 20px !important;' +
-  '	}' +
-  '}' +
+    '#footcnt ._hd,' +
+    '#footcnt ._iq {' +
+    '    margin-left: 0 !important;' +
+    '}' +
 
-  '#cnt {' +
-  '	background: transparent !important;' +
-  '}' +
+    '/* Align image search results. */' +
 
-  '#rg {' +
-  '	position: relative !important;' +
-  '	width: 100% !important;' +
-  '}' +
+    '#irc_bg {' +
+    '    -webkit-box-sizing: border-box !important;' +
+    '       -moz-box-sizing: border-box !important;' +
+    '            box-sizing: border-box !important;' +
+    '    left: 0 !important;' +
+    '    padding-left: 200px !important;' +
+    '    background: transparent !important;' +
+    '}' +
 
-  '#rhscol {' +
-  '	overflow: visible !important;' +
-  '}' +
+    '#irc_cl,' +
+    '#irc_cc {' +
+    '    background-color: #222 !important;' +
+    '}' +
 
-  '#irc_bg {' +
-  '	-webkit-box-sizing: border-box !important;' +
-  '	   -moz-box-sizing: border-box !important;' +
-  '	        box-sizing: border-box !important;' +
-  '	left: 0 !important;' +
-  '	padding-left: 200px !important;' +
-  '	background: transparent !important;' +
-  '}' +
+    '#irc_cc {' +
+    '    margin-left: -200px !important;' +
+    '}' +
 
-  '#irc_cl,' +
-  '#irc_cc {' +
-  '	background-color: #222 !important;' +
-  '}' +
+    '/* Align Wikipedia block on right. */' +
 
-  '#ucs > div.uc {' +
-  '	margin-left: 215px !important;' +
-  '	padding: 5px !important;' +
-  '}' +
+    '#rhscol {' +
+    '    margin-left: 200px !important;' +
+    '}' +
 
-  '#fbar > div.fbar,' +
-  '#footcnt {' +
-  '	margin-left: 215px !important;' +
-  '	background: none !important;' +
-  '}' +
+    '#rhs_block {' +
+    '    margin-left: -100px !important;' +
+    '}' +
 
-  '#fbar > div.fbar > span.fbarspacing {' +
-  '	margin-left: 0 !important;' +
-  '}' +
+    '/* Align "People also search for..." banner. */' +
 
-  '#gsr:not(.hp) #gbqf,' +
-  '#gsr:not(.hp) #tsf .tsf-p > div > table:first-child {' +
-  '	margin-left: 90px !important;' +
-  '}' +
+    '#botabar {' +
+    '    margin-left: 15px !important;' +
+    '}' +
 
-  '#rhs {' +
-  '	overflow: visible !important;' +
-  '	margin-left: 820px !important;' +
-  '}' +
+    '#kappbar {' +
+    '    margin-left: 200px !important;' +
+    '}' +
 
-  '#botabar ul.klcar {' +
-  '	margin-left: 0 !important;' +
-  '}'
-);
+    '#kappbar .klcar {' +
+    '    margin-left: 0 !important;' +
+    '}' +
+
+    '/* Align top search form. */' +
+
+    '#gsr:not(.hp) #tsf .tsf-p {' +
+    '    padding-left: 0 !important;' +
+    '}' +
+
+    '#gsr:not(.hp) #tsf .tsf-p > div > table:first-child {' +
+    '    margin-left: 200px !important;' +
+    '    padding-left: 10px !important;' +
+    '}' +
+
+    '#gsr:not(.hp) #gbq1 {' +
+    '    min-width: 200px !important;' +
+    '    max-width: 200px !important;' +
+    '    margin-right: 15px !important;' +
+    '    padding-right: 0 !important;' +
+    '}' +
+
+    '/**' +
+    ' * Action menu - Display all links.' +
+    ' */' +
+
+    '.action-menu .ab_button {' +
+    '    display: none !important;' +
+    '}' +
+
+    '.action-menu .action-menu-panel {' +
+    '    display: inline-block !important;' +
+    '    visibility: inherit !important;' +
+    '    position: relative !important;    ' +
+    '    top: 0 !important;' +
+    '    z-index: 0 !important;' +
+    '    border: 0 !important;' +
+    '    box-shadow: none !important;' +
+    '    background: transparent !important;' +
+    '    vertical-align: top !important;' +
+    '}' +
+
+    '.action-menu .action-menu-panel .action-menu-item {' +
+    '    display: inline-block !important;' +
+    '    margin: 0 2px !important;' +
+    '    padding: 0 5px !important;' +
+    '    background: #EEE !important;' +
+    '    font-size: 11px !important;' +
+    '}' +
+
+    '.action-menu .action-menu-panel .action-menu-item a.fl {' +
+    '    padding: 0 !important;' +
+    '    font-size: 11px !important;' +
+    '}' +
+
+    '.action-menu .action-menu-panel .action-menu-button {' +
+    '    padding: 0 !important;' +
+    '}' +
+
+    '/**' +
+    ' * Title links - Normal underline.' +
+    ' */' +
+
+    '#rcnt h3.r a,' +
+    '#rcnt span.tl a {' +
+    '    text-decoration: underline !important;' +
+    '}' +
+
+    '/**' +
+    ' * Keywords - Red highlight.' +
+    ' */' +
+
+    '#rcnt h3.r em,' +
+    '#rcnt span.tl em {' +
+    '    color: #DD4B39 !important;' +
+    '    font-weight: bold !important;' +
+    '}';
+
+    if (isFunction(GM_addStyle)) {
+      GM_addStyle(css);
+    } else if (isFunction(PRO_addStyle)) {
+      PRO_addStyle(css);
+    } else if (isFunction(addStyle)) {
+      addStyle(css);
+    }
+
+    function isFunction(func) {
+      return Object.prototype.toString.call(func) === '[object Function]';
+    }
+
+    function addStyle(css) {
+      var heads = document.getElementsByTagName('head');
+      var node = document.createElement('style');
+      node.type = 'text/css';
+      node.appendChild(document.createTextNode(css));
+
+      if (heads.length > 0) {
+        heads[0].appendChild(node);
+      } else {
+        document.documentElement.appendChild(node);
+      }
+    }
+}());
